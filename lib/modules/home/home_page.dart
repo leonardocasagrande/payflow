82d03040f496ecfb7a0b24f175fd3026f3eeeb04
Leonardo Casagrande <leonardo.casagrande@hitechnologies.com.br>
2021-06-25 15:00:20 -0300

    Finished project

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/extract/extract_page.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/modules/my_boletos/my_boletos_page.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class HomePage extends StatefulWidget {
+  final UserModel user;
+  const HomePage({Key? key, required this.user}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: [
+        MyBoletosPage(
+          key: UniqueKey(),
+        ),
+        ExtractPage(
+          key: UniqueKey(),
+        )
+      ][controller.currentPage],
+      appBar: PreferredSize(
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: widget.user.name,
+                          style: TextStyles.titleBoldBackground)
+                    ]),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                width: 48,
+                height: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                    image: DecorationImage(
+                        image: NetworkImage(widget.user.photoURL!))),
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(152),
+      ),
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(Icons.home),
+                color: controller.currentPage == 0
+                    ? AppColors.primary
+                    : AppColors.body),
+            Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: IconButton(
+                  onPressed: () async {
+                    await Navigator.pushNamed(context, "/barcode_scanner");
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.add_box_outlined),
+                  color: AppColors.background,
+                )),
+            IconButton(
+                onPressed: () {
+                  controller.setPage(1);
+                  setState(() {});
+                },
+                icon: Icon(Icons.description_outlined),
+                color: controller.currentPage == 1
+                    ? AppColors.primary
+                    : AppColors.body)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/my_boletos/my_boletos_page.dart
@@ -0,0 +1,67 @@
+import 'package:animated_card/animated_card.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/boleto_info/boleto_info.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
+
+class MyBoletosPage extends StatefulWidget {
+  const MyBoletosPage({Key? key}) : super(key: key);
+
+  @override
+  _MyBoletosPageState createState() => _MyBoletosPageState();
+}
+
+class _MyBoletosPageState extends State<MyBoletosPage> {
+  final controller = BoletoListController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          AnimatedCard(
+            direction: AnimatedCardDirection.top,
+            child: Stack(
+              children: [
+                Container(
+                  color: AppColors.primary,
+                  height: 40,
+                  width: double.maxFinite,
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: ValueListenableBuilder<List<BoletoModel>>(
+                      valueListenable: controller.boletosNotifier,
+                      builder: (_, boletos, __) =>
+                          BoletoInfo(size: boletos.length),
+                    )),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: Row(
+              children: [
+                Text(
+                  "Meus boletos",
+                  style: TextStyles.titleBoldHeading,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Divider(thickness: 1, height: 1, color: AppColors.stroke),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: BoletoList(controller: controller),
+          )
+        ],
+      ),
+    );
+  }
+}
